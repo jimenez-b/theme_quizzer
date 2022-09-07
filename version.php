b599ec084e4108c252e6bca40ad5ef4ddcf7eab3
Brandon <brandon.jimenez@concordia.ca>
2022-09-07 14:58:37 -0400

    Update version.php

--- version.php
+++ version.php
@@ -16,18 +16,12 @@
 
 // This line protects the file from being accessed by a URL directly.                                                               
 defined('MOODLE_INTERNAL') || die();                                                                                                
- 
-// This is the version of the plugin.                                                                                               
-$plugin->version = '2016102100';                                                                                                    
- 
-// This is the version of Moodle this plugin requires.                                                                              
-$plugin->requires = '2016070700';                                                                                                   
- 
-// This is the component name of the plugin - it always starts with 'theme_'                                                        
-// for themes and should be the same as the name of the folder.                                                                     
-$plugin->component = 'theme_quizzer';                                                                                                 
- 
+                                                                                             
+$plugin->version   = '2016102100';     // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = '2016070700';     // Requires this Moodle version.
+$plugin->component = 'theme_quizzer';  // Full name of the plugin (used for diagnostics).
+$plugin->release   = 'Version 0.0.2';  // Version name in human form
 // This is a list of plugins, this plugin depends on (and their versions).                                                          
-$plugin->dependencies = [                                                                                                           
-    'theme_boost' => '2020061500'                                                                                                   
+$plugin->dependencies = [
+    'theme_boost' => '2020061500'
 ];
